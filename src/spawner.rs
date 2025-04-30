f734196832a108f46f2bd2487763f11360db506d
Jonathan Alter <jaltaire@gmail.com>
2025-04-29 23:51:23 -0500

    Revert "remove dependency on tokio:rt-multi-threaded"
    This reverts commit 0931e83bd519bdb5e4321562e46bb860ebe61fbf.

--- src/spawner.rs
+++ src/spawner.rs
@@ -78,10 +78,12 @@ pub mod use_tokio {
 
     unsafe impl Blocker for Tokio {
         fn block_on<T, F: Future<Output = T>>(&self, f: F) -> T {
-            tokio::runtime::Builder::new_current_thread()
-                .build()
-                .unwrap()
-                .block_on(f)
+            tokio_task::block_in_place(|| {
+                tokio::runtime::Builder::new_current_thread()
+                    .build()
+                    .unwrap()
+                    .block_on(f)
+            })
         }
     }
 }
